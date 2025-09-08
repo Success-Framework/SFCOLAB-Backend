5bc70758ccf28593f5074fe001780a9c0eaf2a40
reek004 <chatterjeeswarnabha@gmail.com>
2025-09-08 22:11:21 +0530

    Update CORS configuration in server.js to allow access from new local development ports 5173 and 5174.

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.use(helmet());
 app.use(cors({
   origin: process.env.NODE_ENV === 'production' 
     ? ['https://yourdomain.com'] 
-    : ['http://localhost:3000', 'http://localhost:3001'],
+    : ['http://localhost:5173', 'http://localhost:5174'],
   credentials: true
 }));
 
